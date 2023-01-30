6fc7da47206aa89621b5f0eaf8c44d6638cd73c0
sharmila <48220@edu.raseko.fi>
2023-01-30 10:13:41 +0200

    Update firstfile.py
    Created class Car with 3 attributes registation number, color, model

--- firstfile.py
+++ firstfile.py
@@ -1,1 +1,14 @@
-print("Sharmila")
+class Car:
+    def __init__(self, registrationnumber, color, model):
+        self.registrationnumber = registrationnumber
+        self.color = color
+        self.model = model
+
+    def printname(self):
+        print(self.registrationnumber, self.color, self.model)
+
+# Use the Person class to create an object, and then execute the printname method:
+
+
+x = Car("1212", "Red", "Tyota")
+x.printname()
